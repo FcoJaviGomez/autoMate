b7b3b319c125d215e4513524a184eb08525e5264
miguedeabreu <miguedeabreu@gmail.com>
2022-03-29 10:51:13 +0200

    subida rama home logueado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './component/login/login.component';
 import { PaginaLoginComponent } from './pages/pagina-login/pagina-login.component';
 import { Home1Component } from './pages/home1/home1.component';
+import { Home2Component } from './pages/home2/home2.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     PaginaLoginComponent,
-    Home1Component
+    Home1Component,
+    Home2Component
   ],
   imports: [
     BrowserModule,
