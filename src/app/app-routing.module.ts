abd0cb52baff97b4bebec3c1b786f64409160704
pabloCodenotch <seriusmail13@gmail.com>
2022-03-29 12:29:33 +0200

    Confirmando los cambios y las subidas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,7 @@ import { Home1Component } from './pages/home1/home1.component';
 
 const routes: Routes = [
   {path:"kilometraje", component: KilometrajeComponent},
-  {path: "home1", component: Home1Component}
+  {path: "", component: Home1Component}
 ]
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
