a7d14718abaa4258a3e8dafcde87efcc188ac14b
pabloCodenotch <seriusmail13@gmail.com>
2022-03-30 13:55:18 +0200

    Haciendo los routing existentes en este momento

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,14 +6,22 @@ import { PaginaLoginComponent } from './pages/pagina-login/pagina-login.componen
 import { PaginaRegistroComponent } from './pages/pagina-registro/pagina-registro.component';
 import { HistorialComponent } from './pages/historial/historial.component';
 import { PaginaMantenimientoComponent } from './pages/pagina-mantenimiento/pagina-mantenimiento.component';
+import { PaginaRecuperacionComponent } from './pages/pagina-recuperacion/pagina-recuperacion.component';
+import { PaginaPerfilComponent } from './pages/pagina-perfil/pagina-perfil.component';
+import { PaginaMisDatosComponent } from './pages/pagina-mis-datos/pagina-mis-datos.component';
+import { InfoAppComponent } from './pages/info-app/info-app.component';
 
 const routes: Routes = [
   { path: "", component: Home1Component},
   { path: "pagina-login", component: PaginaLoginComponent },
   { path: "pagina-registro", component: PaginaRegistroComponent },
-  { path:"kilometraje", component: KilometrajeComponent},
+  { path: "kilometraje", component: KilometrajeComponent},
   { path: "historial", component: HistorialComponent},
-  { path: "mantenimiento", component: PaginaMantenimientoComponent}
+  { path: "mantenimiento", component: PaginaMantenimientoComponent},
+  { path: "recuperacion", component: PaginaRecuperacionComponent},
+  { path: "perfil", component: PaginaPerfilComponent},
+  { path: "mis-datos", component: PaginaMisDatosComponent},
+  { path: "info-app", component: InfoAppComponent}
 ];
 
 
