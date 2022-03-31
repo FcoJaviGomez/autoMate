5f55b91a6e883a77a650e81d88c2a0d0751f3175
pabloCodenotch <seriusmail13@gmail.com>
2022-03-31 16:51:39 +0200

    La funcionalidad del header y el footer ha sido completada e implementada

--- src/app/pages/pagina-perfil/pagina-perfil.component.ts
+++ src/app/pages/pagina-perfil/pagina-perfil.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { UsuarioService } from 'src/app/servicios/usuario.service';
 
 @Component({
   selector: 'app-pagina-perfil',
@@ -7,7 +10,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PaginaPerfilComponent implements OnInit {
 
-  constructor() { }
+  constructor(public usuario: UsuarioService, public router: Router) { }
+
+  cerrarSesion()
+  {
+    this.usuario.logged= false;
+    this.router.navigate(['/'])
+  }
 
   ngOnInit(): void {
   }
