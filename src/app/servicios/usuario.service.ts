ae20697256232302cc55281dcdd93c6b4e002dc2
FcoJavierGómez <javiergomez742@gmail.com>
2022-04-01 13:10:04 +0200

    Subida arrego focus iconos

--- src/app/servicios/usuario.service.ts
+++ src/app/servicios/usuario.service.ts
@@ -7,9 +7,13 @@ export class UsuarioService {
 
   public logged: boolean
 
-  constructor() { 
+  public iconoPerfil: boolean
 
+  public iconoLlave: boolean
+
+  constructor() {
     this.logged = false;
-
+    this.iconoPerfil = false
+    this.iconoLlave = false
   }
 }
