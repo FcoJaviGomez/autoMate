c7a0879dfa2eb1abe47304cf0081a5c5f5057574
Olga Grusza <olgagrusza@gmail.com>
2022-04-11 10:58:49 +0200

    subida de funcionalidad de media kilometraje y año mas varios arreglos de css

--- src/app/component/media-kilometraje/media-kilometraje.component.ts
+++ src/app/component/media-kilometraje/media-kilometraje.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Usuario } from 'src/app/models/usuario';
+import { UsuarioService } from 'src/app/servicios/usuario.service';
 
 @Component({
   selector: 'app-media-kilometraje',
@@ -7,8 +9,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MediaKilometrajeComponent implements OnInit {
 
-  constructor() { }
+  public usuario: Usuario
 
+  constructor(public usuarioService: UsuarioService) 
+  {
+    this.usuario = this.usuarioService.usuario
+    console.log(this.usuario);
+    console.log(this.usuarioService.usuario)
+  }
+ modificar(kilometros:number, año:number)
+ {
+   this.usuario = new Usuario(this.usuario.id_user, this.usuario.name, this.usuario.last_name, this.usuario.email, this.usuario.password, kilometros, año, this.usuario.provisional_password, this.usuario.provisional_date)
+   console.log(this.usuario)
+   this.usuarioService.putUsuario(this.usuario).subscribe((data:Usuario) =>
+   {
+     console.log(data);
+     this.usuario = data;
+     this.usuario.year_car  = año
+     this.usuario.kilometers_car = kilometros
+
+     console.log(this.usuario)
+   })
+ }
   ngOnInit(): void {
   }
 
