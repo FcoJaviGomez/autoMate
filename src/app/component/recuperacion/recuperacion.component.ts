bea285b2985d345cbc1862b396f9e3ef30fee450
pabloCodenotch <seriusmail13@gmail.com>
2022-04-18 11:51:24 +0200

    funcionalidad recuperacion completa

--- src/app/component/recuperacion/recuperacion.component.ts
+++ src/app/component/recuperacion/recuperacion.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { Usuario } from 'src/app/models/usuario';
+import { ContraseñaService } from 'src/app/servicios/contraseña.service';
 
 @Component({
   selector: 'app-recuperacion',
@@ -8,8 +11,14 @@ import { Router } from '@angular/router';
 })
 export class RecuperacionComponent implements OnInit {
 
-  constructor(public router: Router) { }
-  recuperar() {
+  constructor(public router: Router, public contrasenya: ContraseñaService) { }
+  recuperar(email: HTMLInputElement) {
+
+    let correo = new Usuario(0,null,null,email.value,null,0,0,null,null);
+    console.log(correo)
+    this.contrasenya.restoreContrasena(correo).subscribe(function(data){
+      console.log(data)
+    })
     this.router.navigate(['/pagina-login'])
   }
 
---
+++ src/app/servicios/contraseña.service.ts
@@ -0,0 +1,25 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Usuario } from '../models/usuario';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ContraseñaService {
+
+  // private url = "https://my-api-automate.herokuapp.com"
+
+  
+  private url = "http://localhost:3000"
+
+  constructor(private http: HttpClient) { }
+
+  restoreContrasena(contrasena: Usuario):Observable<Object>{
+
+    return this.http.put(this.url + '/recuperacion', contrasena )
+
+  }
+
+}
